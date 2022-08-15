4594cc08eca6b58583d10dd1fcdbb3f788164d30
Neus <neuscalvo11@gmail.com>
2022-08-15 11:04:55 +0200

    Array con datos

---
+++ Curso-JavaScript/ejercicio2.js
@@ -0,0 +1,12 @@
+const miFecha = new Date(1982, 8 ,6);
+
+const libro = {
+    titulo: "Harry Potter",
+    autor: "Jessica",
+    fecha: 1990,
+    url: "http://libro.com"
+}
+
+const datos = ["Neus", 39, true, miFecha, libro];
+
+console.log(datos);
