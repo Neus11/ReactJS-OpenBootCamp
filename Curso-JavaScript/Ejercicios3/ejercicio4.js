d23b9a6e824ca0a20e25c092fe97f1b4ec948b68
Neus <neuscalvo11@gmail.com>
2022-08-17 10:24:41 +0200

    Trabanjando con cadenas de texto

---
+++ Curso-JavaScript/Ejercicios3/ejercicio4.js
@@ -0,0 +1,29 @@
+let nombre = "Neus";
+
+let apellido = "Calvo";
+
+//let estudiante = nombre + " " +  apellido;
+//let estudiante = `${nombre} ${apellido}`;
+let estudiante = nombre.concat(" ", apellido);
+
+console.log(estudiante);
+
+let estudianteMayus = estudiante.toLocaleUpperCase();
+
+console.log(estudianteMayus);
+
+let estudianteMinus = estudiante.toLocaleLowerCase();
+
+console.log(estudianteMinus);
+
+let caracteres = estudiante.length;
+
+console.log(caracteres);
+
+let primeraLetra = nombre.charAt(0);
+console.log(primeraLetra);
+
+let ultimaLetra = apellido.charAt(4);
+console.log(ultimaLetra);
+
+console.log(estudiante.includes(nombre));
