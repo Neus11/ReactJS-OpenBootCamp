33bb631c3910f77940c4fbeeb5ab7d9c3180aca6
Neus <neuscalvo11@gmail.com>
2022-08-18 16:56:47 +0200

    Ejercicio Conjuntos

---
+++ Curso-JavaScript/ejercicio7/conjuntos.js
@@ -0,0 +1,10 @@
+const arrayFamilia = ["Rosa", "Miguel", "Roser", "Miquel", "Neus"]
+const familiaSet = new Set(arrayFamilia);
+
+familiaSet.add("Neus");
+familiaSet.add("JavaScript");
+
+console.log(familiaSet)
+
+
+
