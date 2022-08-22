f89e5ccc29eb5782dcec1ef6ce08924fcbb6eff3
Neus <neuscalvo11@gmail.com>
2022-08-22 14:34:23 +0200

    ejercicio 12 ESLint

---
+++ Curso-JavaScript/ejercicio12/04-textos.js
@@ -0,0 +1,34 @@
+/* eslint-disable spaced-comment */
+/* eslint-disable prefer-const */
+let nombre = 'Neus'
+
+let apellido = 'Calvo'
+
+//let estudiante = nombre + " " +  apellido;
+//let estudiante = `${nombre} ${apellido}`;
+let estudiante = nombre.concat(' ', apellido)
+
+console.log(estudiante)
+
+let estudianteMayus = estudiante.toLocaleUpperCase()
+
+console.log(estudianteMayus)
+
+let estudianteMinus = estudiante.toLocaleLowerCase()
+
+console.log(estudianteMinus)
+
+let caracteres = estudiante.length
+
+console.log(caracteres)
+
+let primeraLetra = nombre.charAt(0)
+console.log(primeraLetra)
+
+let ultimaLetra = apellido.charAt(4)
+console.log(ultimaLetra)
+
+let sinEspacios = estudiante.replace(' ', '')
+console.log(sinEspacios)
+
+console.log(estudiante.includes(nombre))
