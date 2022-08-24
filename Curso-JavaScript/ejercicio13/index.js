76197400f0c3198adcef23f53fc6a5e93b15b92a
Neus <neuscalvo11@gmail.com>
2022-08-24 10:48:32 +0200

    ejercicio 13 Event

---
+++ Curso-JavaScript/ejercicio13/index.js
@@ -0,0 +1,10 @@
+const btn = document.getElementsByTagName("button")[0]
+
+btn.addEventListener("click", () => {
+    alert("Click en el boton")
+})
+
+
+$("#btnn").click(function() {
+    console.log("Hola estoy utilizando JQuery")
+})
